bac730ed5e180d74df647ccb7e8ef4e104e280d2
Wharem85-25 <wharem85@hotmail.com>
2022-11-07 15:01:24 -0600

    se agregaron correos

--- nodemailer.js
+++ nodemailer.js
@@ -1,12 +1,9 @@
 const nodemailer = require("nodemailer");
 
-// async..await is not allowed in global scope, must use a wrapper
 async function sendMail() {
-
-  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    secure: true, // true for 465, false for other ports
+    secure: true,
     port: 465,
     auth: {
         user: '',
@@ -14,21 +11,17 @@ async function sendMail() {
     }
   });
 
-  // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '@gmail.coml', // sender address
-    to: "@hotmail.com", // list of receivers
-    subject: "Este es un nuevo correo", // Subject line
-    text: "Hola", // plain text body
-    html: "<b>Hola puta queres que te la ponga</b>", // html body
+    from: '@gmail.coml',
+    to: "@hotmail.com",
+    subject: "Este es un nuevo correo",
+    text: "Hola",
+    html: "<b>Hola</b>",
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 sendMail();
